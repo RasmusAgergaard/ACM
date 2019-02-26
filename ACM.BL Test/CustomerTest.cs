4ccf5d51047c27ebd64853c56cdc572af8f6ab3e
RasmusAgergaard <rasmusap@gmail.com>
2019-02-26 07:52:49 +0100

    Project created

---
+++ ACM.BL Test/CustomerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL_Test
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+
+            string expected = "Baggins, Bilbo";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+        }
+    }
+}
---
+++ ACM.BL Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BL Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BL Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c756e1eb-6b2a-41e2-9886-971b932989bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
